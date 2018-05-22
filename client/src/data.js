4121b8c467e342e6989678815d853e949490ab18
AntonBrito <tonybrito85@gmail.com>
2018-05-22 00:35:12 -0400

    Added some css.
    Hardcoded data

--- client/src/data.js
+++ client/src/data.js
@@ -1,7 +1,8 @@
 // data.js
 const data = [
   { _id: 1, author: 'Tony', text: 'Wow this is neat', update: new Date(), createdAt: new Date() },
-  { _id: 2, author: 'you' , text: 'Holly crap! You\'re right!_', update: new Date(), createdAt: new Date() },
+  { _id: 2, author: 'Riley' , text: 'Holly crap! You\'re right!_', update: new Date(), createdAt: new Date() },
+  { _id: 3, author: 'Jessica' , text: 'Wow! Awesome!', update: new Date(), createdAt: new Date() },
 ];
 
 export default data;
